0e484b9351010c1a6c1197f4018503457fa52590
Camilo García <saldarriagagarciacamilo@gmail.com>
2023-05-15 11:46:58 -0500

    Update Front

---
+++ static/js/demo/chart-pie-demo.js
@@ -0,0 +1,43 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var variable2 = JSON.parse(ctx.getAttribute("data-variables2"));
+var labels2 = variable2.map(function(item) {
+  return item.Nombre;
+});
+
+var data2 = variable2.map(function(item) {
+  return item.cantidad;
+});
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: labels2,
+    datasets: [{
+      data: data2,
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: true
+    },
+    cutoutPercentage: 80,
+  },
+});
